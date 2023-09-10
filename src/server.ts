20e2f25b93997a7a8b16729e73e1107e54a87139
Karllos26 <karlloseduardopereira@gmail.com>
2023-09-10 19:07:03 -0300

    vai logo

--- src/server.ts
+++ src/server.ts
@@ -24,11 +24,12 @@ app.use(express.json()) // habilita o express para receber dados no formato json
 app.use(routes) // habilita as rotas
 app.use(cors()) // habilita o CORS
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://meuapp.com']
-  }))
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
     console.log('Banco de dados', 
     dataBase.isInitialized ? 'inciado' : 'não iniciado')
 })
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://meuapp.com']
+  }))
