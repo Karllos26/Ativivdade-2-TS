1e75cd28ce867a13888158f40b09134e4f288d6d
Karllos26 <karlloseduardopereira@gmail.com>
2023-09-10 19:40:11 -0300

    vamo ver

--- src/routes/task/task.routes.ts
+++ src/routes/task/task.routes.ts
@@ -4,9 +4,8 @@ import TaskController from '../../controllers/task/task.controller'
 const taskRoutes = Router()
 
 taskRoutes.post('/', TaskController.store)
-
-export default taskRoutes
 taskRoutes.get('/', TaskController.index)
 taskRoutes.get('/:id', TaskController.show)
 taskRoutes.delete('/:id', TaskController.delete)
 taskRoutes.put('/:id', TaskController.update) // usamos o put para atualizar todos os campos
+export default taskRoutes
