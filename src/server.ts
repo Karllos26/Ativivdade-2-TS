1539bcd751f0d2d6ebe76577f28c1a3ac30c8a89
Karllos26 <karlloseduardopereira@gmail.com>
2023-09-10 18:35:04 -0300

    hora de postar

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import dataBase from './database/ormconfig'
 
 import routes from './routes'
 
+import cors from 'cors'
+
+
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
@@ -19,7 +22,11 @@ res.send({id:1,name:"teste"})
 
 app.use(express.json()) // habilita o express para receber dados no formato json
 app.use(routes) // habilita as rotas
+app.use(cors()) // habilita o CORS
 
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://meuapp.com']
+  }))
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
     console.log('Banco de dados', 
