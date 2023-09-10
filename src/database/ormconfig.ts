14c5c9035c4ab47bcc06d682fc100fd7c8d8e1d7
Karllos26 <karlloseduardopereira@gmail.com>
2023-09-10 18:48:27 -0300

    ainda com um erro

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,15 +1,16 @@
 import{DataSource} from "typeorm"
 import dotenv from "dotenv"
+import { join } from "path"
 
 const dataBase = new DataSource({
-    type: 'sqlite',
-    database: process.env.DATABASE || './src/database/db.sqlite',
-    entities: [ 
+    type:'sqlite',
+    database: process.env.DATABASE || '/db.sqlite',
+    entities: [
       join(__dirname, '..', 'models/*.{ts,js}')
     ],
     logging: true,
     synchronize: true
-  })
+})
 
 dataBase.initialize()
 .then(() => {
